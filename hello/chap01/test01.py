47b4c753b460d555e82bdb8e309f7f02fc2c46ea
黄卉 <huanghui@huanghuideMacBook-Pro.local>
2016-05-09 21:52:42 +0800

    hello

---
+++ hello/chap01/test01.py
@@ -0,0 +1,11 @@
+# -*- encoding:utf-8 -*-
+print 'hello word' ;
+
+print '33 * 33= ', 33 * 33;
+print '2+2=',2+2;
+# 除法需要换成浮点数
+print '1/2=',1/2;
+print '1.0/2.0=',1.0/2.0;
+
+name = raw_input('What is your name?')
+print 'hello, '+name+'!'
