fc57ece0fdc4dd46fd1667f9e0edad72ea08ea06
黄卉 <326028030@qq.com>
2016-05-12 08:42:42 +0800

    第二章开始

---
+++ hello/chap02/test02.py
@@ -0,0 +1,17 @@
+# -*- encoding:utf-8 -*-
+#第二章 列表和元组
+
+#序列
+e = ['Edward Gumby' , 42]
+j = ['John Smith' , 50 ]
+database = [e,j]
+print database
+
+#索引
+gstr = 'Hello'
+print gstr[0]
+print gstr[-1]
+print 'Hello'[1]
+
+fourth = raw_input('Year:')[3]
+print fourth
