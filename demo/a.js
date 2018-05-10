811e567e8da7c7aa10d260a9d9652d0b81aa4784
黄卉 <huanghui8030@qq.com>
2018-05-11 07:04:08 +0800

    实现有序数组相加后也为一个有序数组

--- demo/a.js
+++ demo/a.js
@@ -1,18 +1,32 @@
 /**
- * 实现两个有序数组相加后，为一个有有序数据。
+ * 实现两个有序数组相加后，为一个有序数组。
  * var a = [1,3,5,7,9],
  *     b = [2,6,8,10]
  */
-function arrFn(aArr,bArr){
-	var newArr = bArr;
+function arrChangeFn(aArr,bArr){
+	console.log("输入的数组：",aArr,bArr);
 	for (var i = 0 ; i < aArr.length ; i++) {
 		for (var j = 0; j < bArr.length; j++) {
-			
-			if(arr[i] > brr[j]){
-
+			if(aArr[i] < bArr[j]){
+				if(bArr[j] < aArr[i+1]){
+					aArr.splice(i+1,0,bArr[j]);
+					i = i+1
+					break;
+				}else if(i==aArr.length-1 && j === bArr.length-1){
+					//console.log(i,j);
+ 					aArr.push(bArr[j]);
+ 					break;
+				}
+			}else if(i===0){
+				aArr.splice(i,0,bArr[i]);
+				i = i+1;
+				break;
 			}
-
 		}
 	}
-	return newArr;
+	console.log("生成的新数组：",aArr);
+	return aArr;
 }
+
+arrChangeFn([1,3,5,7,9 ],[2,6,8,10]);
+arrChangeFn([3,6,7,8,12],[1,2,5,9,10]);
