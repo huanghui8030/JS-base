4fba6e8216484c21e7476e908fcfbc0e5670fde2
黄卉 <326028030@qq.com>
2018-05-28 08:40:41 +0800

    string方法

--- base/string.js
+++ base/string.js
@@ -1,2 +1,14 @@
 var a = 'Hello world!';
 console.log(a.concat('哈哈哈！'),a);
+
+var  arr = [1,2,3];
+console.log(arr,JSON.stringify(arr));//[ 1, 2, 3 ] '[1,2,3]'
+console.log(arr.join());
+
+var str = '[1,2,3]' ;
+console.log(str,JSON.parse(str));
+
+var str1 = 'a-b-c'
+console.log(str1.split(''));
+
+
